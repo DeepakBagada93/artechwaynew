9f633b3a57f3aa3583f7cd7ebdfc5035c73fa251
Deepak Bagada <deeepakbagada25@gmail.com>
2025-08-18 02:58:27 +0000

    use the comfortaa font in the whole website

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"Space Grotesk"', 'sans-serif'],
+        body: ['"Comfortaa"', 'sans-serif'],
+        headline: ['"Comfortaa"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
